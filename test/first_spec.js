62e51ba2b6d734b5c30fda385625cbc955ffd2eb
Lee Jun Yeong <leejunyeong@lees-mbp.woo.sing.seagate.com>
2017-10-03 16:00:51 +0800

    Initial load

---
+++ test/first_spec.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('A Feature', function () {
+    describe('A Scenario', function () {
+        it('does something expected', function(){
+            assert(true);
+        });
+    });
+});
